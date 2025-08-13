9777825ddadc265dc462b98e476f9aed995f6cf9
varubogu <toyosuke@varubogu.com>
2025-08-13 15:25:21 +0900

    refactor: コマンド登録時、それぞれ関数をuseするのではなく、直接コマンドのフルの名前を指定

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,124 +6,67 @@ mod models;
 mod types;
 mod errors;
 
-use commands::{
-    account_commands::{
-        create_account,
-        get_account,
-        update_account,
-        delete_account,
-    },
-    initialization_commands::{
-        load_local_settings,
-        load_current_account,
-        load_all_project_data,
-        load_all_account,
-    },
-    project_commands::{
-        create_project,
-        get_project,
-        update_project,
-        delete_project,
-        search_projects,
-    },
-    setting_commands::{
-        get_setting,
-        get_all_settings,
-        update_setting,
-    },
-    subtask_commands::{
-        // Aliases for frontend compatibility
-        create_sub_task,
-        get_sub_task,
-        update_sub_task,
-        delete_sub_task,
-        search_sub_tasks,
-    },
-    tag_commands::{
-        create_tag,
-        get_tag,
-        update_tag,
-        delete_tag,
-        search_tags,
-    },
-    task_commands::{
-        create_task,
-        get_task,
-        update_task,
-        delete_task,
-        search_tasks,
-    },
-    task_list_commands::{
-        create_task_list,
-        get_task_list,
-        update_task_list,
-        delete_task_list,
-        search_task_lists,
-    },
-};
+use commands::*;
 
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
 
     // Ensure directories are created at startup
-
-
-
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
             // Initialization commands
-            load_local_settings,
-            load_current_account,
-            load_all_project_data,
-            load_all_account,
-            
+            initialization_commands::load_local_settings,
+            initialization_commands::load_current_account,
+            initialization_commands::load_all_project_data,
+            initialization_commands::load_all_account,
+
             // Account commands
-            create_account,
-            get_account,
-            update_account,
-            delete_account,
+            account_commands::create_account,
+            account_commands::get_account,
+            account_commands::update_account,
+            account_commands::delete_account,
 
             // Task management commands
-            create_task,
-            get_task,
-            update_task,
-            delete_task,
-            search_tasks,
+            task_commands::create_task,
+            task_commands::get_task,
+            task_commands::update_task,
+            task_commands::delete_task,
+            task_commands::search_tasks,
 
             // Project management commands
-            create_project,
-            get_project,
-            update_project,
-            delete_project,
-            search_projects,
+            project_commands::create_project,
+            project_commands::get_project,
+            project_commands::update_project,
+            project_commands::delete_project,
+            project_commands::search_projects,
 
             // Setting management commands
-            get_setting,
-            get_all_settings,
-            update_setting,
+            setting_commands::get_setting,
+            setting_commands::get_all_settings,
+            setting_commands::update_setting,
 
             // Subtask management commands (frontend compatibility aliases)
-            create_sub_task,
-            get_sub_task,
-            update_sub_task,
-            delete_sub_task,
-            search_sub_tasks,
+            subtask_commands::create_sub_task,
+            subtask_commands::get_sub_task,
+            subtask_commands::update_sub_task,
+            subtask_commands::delete_sub_task,
+            subtask_commands::search_sub_tasks,
 
             // Tag management commands
-            create_tag,
-            get_tag,
-            update_tag,
-            delete_tag,
-            search_tags,
+            tag_commands::create_tag,
+            tag_commands::get_tag,
+            tag_commands::update_tag,
+            tag_commands::delete_tag,
+            tag_commands::search_tags,
 
             // TaskList commands
-            create_task_list,
-            get_task_list,
-            update_task_list,
-            delete_task_list,
-            search_task_lists,
+            task_list_commands::create_task_list,
+            task_list_commands::get_task_list,
+            task_list_commands::update_task_list,
+            task_list_commands::delete_task_list,
+            task_list_commands::search_task_lists,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
